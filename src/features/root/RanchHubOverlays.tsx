75e550c6c007838d727329e4e14bda462f5e0594
slickgame <slickpokefangame@gmail.com>
2026-06-27 19:51:39 -0400

    Add ranch hub overlays component

---
+++ src/features/root/RanchHubOverlays.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ChapterOneGuidePanel } from "@/features/ranch/ChapterOneGuidePanel";
+import { DailyReportOverlayCards } from "@/features/ranch/DailyReportOverlayCards";
+import { RanchAdvisorOverlay } from "@/features/ranch/RanchAdvisorOverlay";
+import { useGameContext } from "@/state/GameProvider";
+
+export function RanchHubOverlays() {
+  const { appScreen, currentSave } = useGameContext();
+  if (!currentSave || appScreen !== "ranch-hub") return null;
+  return <><RanchAdvisorOverlay /><ChapterOneGuidePanel /><DailyReportOverlayCards /></>;
+}
