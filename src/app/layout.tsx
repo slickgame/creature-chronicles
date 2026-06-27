062885595a66985e215a703cf2d2f15d645b885a
slickgame <slickpokefangame@gmail.com>
2026-06-27 19:50:50 -0400

    Remove advisor offset stylesheet import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./guild-board.css";
-import "./advisor-offset.css";
 import { GameProvider } from "@/state/GameProvider";
 
 export const metadata: Metadata = {
