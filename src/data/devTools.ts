516662ebfe0160ad3c2422063528bc36890cc324
slickgame <slickpokefangame@gmail.com>
2026-06-27 12:51:08 -0400

    Recreate dev tools export

---
+++ src/data/devTools.ts
@@ -0,0 +1,2 @@
+export { createDevCreature, createDevEgg, grantDevResources, resetDevGuild, resetDevMarket, resetDevRanchUpgrades } from "./devToolsSafe";
+export type { DevActionResult } from "./devToolsSafe";
