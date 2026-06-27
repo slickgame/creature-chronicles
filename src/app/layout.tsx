26227efeeac0521f1f1704bc1d8c012c1f8fab13
slickgame <slickpokefangame@gmail.com>
2026-06-27 15:35:27 -0400

    Load advisor offset stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./guild-board.css";
+import "./advisor-offset.css";
 import { GameProvider } from "@/state/GameProvider";
 
 export const metadata: Metadata = {
