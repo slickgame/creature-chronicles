d4502283a99e15bf4e939834343852e3343df61d
slickgame <slickpokefangame@gmail.com>
2026-06-27 19:51:47 -0400

    Move ranch overlays out of page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-"use client";
-
-import { ChapterOneGuidePanel } from "@/features/ranch/ChapterOneGuidePanel";
-import { DailyReportOverlayCards } from "@/features/ranch/DailyReportOverlayCards";
-import { RanchAdvisorOverlay } from "@/features/ranch/RanchAdvisorOverlay";
 import { GameRoot } from "@/features/root/GameRoot";
-import { useGameContext } from "@/state/GameProvider";
-
-function HomeOverlay() {
-  const { appScreen, currentSave } = useGameContext();
-  if (!currentSave || appScreen !== "ranch-hub") return null;
-  return <><RanchAdvisorOverlay /><ChapterOneGuidePanel /><DailyReportOverlayCards /></>;
-}
+import { RanchHubOverlays } from "@/features/root/RanchHubOverlays";
 
 export default function HomePage() {
-  return <><GameRoot /><HomeOverlay /></>;
+  return <><GameRoot /><RanchHubOverlays /></>;
 }
