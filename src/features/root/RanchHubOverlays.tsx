17680e5f673765cd1008b9a7f4e909d266450e1a
slickgame <slickpokefangame@gmail.com>
2026-06-28 14:56:37 -0400

    Move Veyra trust out of standalone overlay stack

--- src/features/root/RanchHubOverlays.tsx
+++ src/features/root/RanchHubOverlays.tsx
@@ -7,7 +7,6 @@ import { RanchAdvisorOverlay } from "@/features/ranch/RanchAdvisorOverlay";
 import { ChapterOneStoryOverlay } from "@/features/story/ChapterOneStoryOverlay";
 import { StoryImageAdminOverlay } from "@/features/story/StoryImageAdminOverlay";
 import { StoryLogOverlay } from "@/features/story/StoryLogOverlay";
-import { VeyraTrustOverlay } from "@/features/story/VeyraTrustOverlay";
 import { useGameContext } from "@/state/GameProvider";
 
 export function RanchHubOverlays() {
@@ -16,5 +15,5 @@ export function RanchHubOverlays() {
   if (appScreen === "breeding") return <SharedInfoOverlay />;
   if (appScreen === "ranch-office") return <><StoryLogOverlay /><StoryImageAdminOverlay /></>;
   if (appScreen !== "ranch-hub") return null;
-  return <><RanchAdvisorOverlay /><ChapterOneGuidePanel /><DailyReportOverlayCards /><VeyraTrustOverlay /><ChapterOneStoryOverlay /></>;
+  return <><RanchAdvisorOverlay /><ChapterOneGuidePanel /><DailyReportOverlayCards /><ChapterOneStoryOverlay /></>;
 }
