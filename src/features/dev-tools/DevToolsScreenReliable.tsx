096be3d88ac3ce97dc36d4211659a04f04d68b6a
slickgame <slickpokefangame@gmail.com>
2026-07-29 11:19:59 -0400

    Expose structured talent audit from Dev Tools

--- src/features/dev-tools/DevToolsScreenReliable.tsx
+++ src/features/dev-tools/DevToolsScreenReliable.tsx
@@ -2,12 +2,14 @@
 
 import { DevToolsScreen as BaseDevToolsScreen } from "./DevToolsScreen";
 import { SaveReliabilityPanel } from "./SaveReliabilityPanel";
+import { TalentAuditPanel } from "./TalentAuditPanel";
 
 export function DevToolsScreen() {
   return (
     <>
       <BaseDevToolsScreen />
       <SaveReliabilityPanel />
+      <TalentAuditPanel />
     </>
   );
 }
