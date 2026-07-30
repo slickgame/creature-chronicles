37ebf72537fa109f448db6adad38f28da03fb8c5
slickgame <slickpokefangame@gmail.com>
2026-07-30 13:22:11 -0400

    Mount guided Chapter 1 tutorial globally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { GameRoot } from "@/features/root/GameRoot";
 import { RanchHubOverlays } from "@/features/root/RanchHubOverlays";
+import { ChapterOneGuidedTutorial } from "@/features/tutorial/ChapterOneGuidedTutorial";
 
 export default function HomePage() {
-  return <><GameRoot /><RanchHubOverlays /></>;
+  return <><GameRoot /><RanchHubOverlays /><ChapterOneGuidedTutorial /></>;
 }
