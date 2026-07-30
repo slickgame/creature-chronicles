fb4ee1a73f42f14f22199c9c707e0155f2b2d967
slickgame <slickpokefangame@gmail.com>
2026-07-30 13:20:17 -0400

    Connect Battle Outfitter to guided tutorial

---
+++ src/features/battle-outfitter/BattleOutfitterScreenTutorial.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { BattleOutfitterScreenActive as BaseBattleOutfitterScreen } from "./BattleOutfitterScreenC3Active";
+
+export function BattleOutfitterScreenActive() {
+  return (
+    <div data-tutorial-id="tutorial-battle-outfitter">
+      <BaseBattleOutfitterScreen />
+    </div>
+  );
+}
