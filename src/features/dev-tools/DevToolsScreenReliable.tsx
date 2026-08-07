37364f00a52c44f058af6d46e97b350082e54ca6
slickgame <slickpokefangame@gmail.com>
2026-08-07 12:52:21 -0400

    Mount Legacy QA lab in Dev Tools

--- src/features/dev-tools/DevToolsScreenReliable.tsx
+++ src/features/dev-tools/DevToolsScreenReliable.tsx
@@ -5,11 +5,13 @@ import { SaveReliabilityPanel } from "./SaveReliabilityPanel";
 import { TalentAuditPanel } from "./TalentAuditPanel";
 import { BattleMoveAuditPanel } from "./BattleMoveAuditPanel";
 import { PredatorTestPanel } from "./PredatorTestPanel";
+import { LegacyTestPanel } from "./LegacyTestPanel";
 
 export function DevToolsScreen() {
   return (
     <>
       <BaseDevToolsScreen />
+      <LegacyTestPanel />
       <PredatorTestPanel />
       <SaveReliabilityPanel />
       <TalentAuditPanel />
