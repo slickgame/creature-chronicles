a3dd5b71b7e9d8e998efd3c86d40adb050783464
slickgame <slickpokefangame@gmail.com>
2026-08-12 19:52:38 -0400

    Load Egg Registry composition pass 9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ import "./egg-atelier-registry-composition-v5.css";
 import "./egg-atelier-registry-composition-v6.css";
 import "./egg-atelier-registry-composition-v7.css";
 import "./egg-atelier-registry-composition-v8.css";
+import "./egg-atelier-registry-composition-v9.css";
 import { GameProvider } from "@/state/GameProvider";
 
 export const metadata: Metadata = {
